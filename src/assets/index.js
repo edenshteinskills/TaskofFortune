b831b80852a890e0212599fdc3badbee732c1908
edenshteinskills <86909176+edenshteinskills@users.noreply.github.com>
2021-07-28 09:19:07 +0300

    Changed styling (#1)

---
+++ src/assets/index.js
@@ -0,0 +1,3 @@
+import friends_cut from "./audio/friends_cut.mp3";
+
+export { friends_cut };
---
+++ src/database/data.js
@@ -0,0 +1,10 @@
+const data = [
+  { id: 1, option: "Avi" },
+  { id: 2, option: "david" },
+  { id: 3, option: "Eden" },
+  { id: 4, option: "lesvy" },
+  { id: 5, option: "ssss" },
+  { id: 6, option: "Bruce" },
+];
+
+export default data;
